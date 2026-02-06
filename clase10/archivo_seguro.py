3f8a3b69ae94435160ec54fdb3f40bdbe5b895e7
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-02-05 22:28:29 -0300

    added corrections in clase 10

--- clase10/archivo_seguro.py
+++ clase10/archivo_seguro.py
@@ -1,4 +1,4 @@
-import os
+
 print("El archivo a ser leido debe encontrarse en la carpeta de este script")
 ruta = input("Ingrese el nombre del archivo con su extension(.txt,.log,.json, etc): ")
 
@@ -7,18 +7,11 @@ try:
     print("Nombre del archivo: ",archivo.name)
     
     
-        
 except FileNotFoundError:
     print(f"archivo no encontrado, revise su ruta")
 except Exception as e:
     print("Se detecto otro error:" , e)
 
-# else:
-#     print("Intentamos cerrar el archivo")
-
-#     if not archivo.closed:
-#         archivo.close()
-#         print("El archivo se cerro correctamente")
 
 finally:
     try:
